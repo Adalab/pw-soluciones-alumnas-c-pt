ffc1d92f4336f1ee9be6ec7d890e30b9ff5bdca9
Iván (profe) <ivan@adalab.es>
2024-12-10 18:21:34 +0100

    Add a lot of functions.

--- modulo-2/modulo-2-piedra-papel-tijera-con-imagenes-Maria-feat-profe/js/main.js
+++ modulo-2/modulo-2-piedra-papel-tijera-con-imagenes-Maria-feat-profe/js/main.js
@@ -9,12 +9,17 @@ const counterWinParagraph = document.querySelector('.js_counter_win');
 const counterLostParagraph = document.querySelector('.js_counter_lost');
 const winnerParagraph = document.querySelector('.js_winnerParagraph');
 const reloadBtn = document.querySelector('.js_reloadBtn');
-const imagePlayerOptionSelect = document.querySelector('.js_imagePlayerOptionSelect');
-const paragraphPlayerOptionSelect = document.querySelector('.js_paragraphPlayerOptionSelect');
-const imageComputerOptionSelect = document.querySelector('.js_imageComputerOptionSelect');
-const paragraphComputerOptionSelect = document.querySelector('.js_paragraphComputerOptionSelect');
 
-
+const roundMoveElements = {
+  'user': {
+    'image': document.querySelector('.js_imagePlayerOptionSelect'),
+    'paragraph':document.querySelector('.js_paragraphPlayerOptionSelect')
+  },
+  'computer': {
+    'image': document.querySelector('.js_imageComputerOptionSelect'),
+    'paragraph': document.querySelector('.js_paragraphComputerOptionSelect')
+  }
+}
 
 // SECCIÓN DE LOS DATOS DE APLICACIÓN
 
@@ -36,242 +41,197 @@ function getRandomNumber(max) {
 }
 
 const translateNumberToMove = (number) => {
-  
+  if (number >= 1 && number <= 3) {
+    return 'piedra';
+  }
+  else if (number >= 4 && number <= 6) {
+    return 'papel';
+  }
+  else if (number >= 7 && number <= 9) {
+    return 'tijera';
+  }
+
+  return 'ERROR';
 }
 
 const genComputerMove = () => {
   // Generar un número random
 
   const randomNumber = getRandomNumber(9);
-  console.log(randomNumber);
 
   // Registrar lo que la computadora ha generado
-  let computerMove = '';
-
-  if (randomNumber >= 1 && randomNumber <= 3) {
-    computerMove = 'piedra';
-  }
-  else if (randomNumber >= 4 && randomNumber <= 6) {
-    computerMove = 'papel';
-  }
-  else if (randomNumber >= 7 && randomNumber <= 9) {
-    computerMove = 'tijera';
-  }
+  const computerMove = translateNumberToMove(randomNumber);
 
   return computerMove;
 };
 
 
+const getPlayerMove = () => {
+  return userOptionSelect.value;
+}
 
-const handleClickButton = (ev) => {
-  ev.preventDefault();
 
+const whoWins = (userMove, computerMove) => {
+
+  if( userMove === computerMove ) {
+    return 'tie';
+  }
+
+  if( userMove === 'piedra' ) {
+    if( computerMove === 'tijera' ) {
+      return 'user';
+    }
+    else {
+      return 'computer';
+    }
+  }
+  else if( userMove === 'papel' ) {
+    if( computerMove === 'piedra' ) {
+      return 'user';
+    }
+    else {
+      return 'computer';
+    }
+  }
+  else if( userMove === 'tijera' ) {
+    if( computerMove === 'papel' ) {
+      return 'user';
+    }
+    else {
+      return 'computer';
+    }
+  }
+
+  return 'ERROR';
+}
+
+const showMovesElements = () => {
+  roundMoveElements.user.image.classList.remove('hidden');
+  roundMoveElements.user.paragraph.classList.remove('hidden');
+
+  roundMoveElements.computer.image.classList.remove('hidden');
+  roundMoveElements.computer.paragraph.classList.remove('hidden');
+};
+
+const hideMovesElements = () => {
+  roundMoveElements.user.image.classList.add('hidden');
+  roundMoveElements.user.paragraph.classList.add('hidden');
+
+  roundMoveElements.computer.image.classList.add('hidden');
+  roundMoveElements.computer.paragraph.classList.add('hidden');
+};
+
+const getMoveImageSrc = (move) => {
+  return `./images/imagen_${move}.jpg`;
+}
+
+const getMoveImageAlt = (move) => {
+  return `La jugadora ha seleccionado ${move}`;
+}
+
+const getMoveParagraphText = (player, move) => {
+  const playerName = player === 'user' ? 'jugadora' : 'computadora';
+
+  return `La ${playerName} ha seleccionado ${move}`;
+}
+
+const renderMove = (player, move) => {
+  if( !['piedra', 'papel', 'tijera'].includes(move) ) {
+    hideMovesElements();
+
+    return;
+  }
+  showMovesElements();
+  
+  roundMoveElements[player].image.innerHTML = `<img src="${getMoveImageSrc(move)}" alt="${getMoveImageAlt(move)}" />`;
+  roundMoveElements[player].paragraph.innerHTML = `<p class="h2">${getMoveParagraphText(player, move)}</p>`;
+};
+
+const getResultMessage = (winner) => {
+  if( winner === 'user' ) {
+    return 'Has ganado';
+  }
+  else if( winner === 'computer' ) {
+    return 'Has perdido';
+  }
+  else if( winner === 'tie' ) {
+    return 'Has empatado';
+  }
+  else {
+    return 'Recuerda seleccionar una jugada';;
+  }
+}
+
+const renderResult = (winner) => {
+  const message = getResultMessage(winner);
+
+  resultDiv.innerHTML = message;
+}
+
+const incrementCounters = (winner) => {
+  if( winner === 'user' ) {
+    playerWinsCounter++;
+  }
+  else if( winner === 'computer' ) {
+    playerLosesCounter++;
+  }
+}
+
+
+const playRound = () => {
   const computerMove = genComputerMove();
-
-  console.log(computerMove);
-
+  
   // Registrar lo que la usuaria haya escogido
 
-  const userMove = userOptionSelect.value;
+  const userMove = getPlayerMove();
 
-  console.log(userMove);
+  renderMove('computer', computerMove);
+  renderMove('user', userMove);
 
-  const result = compareMoves(userMove, computerMove);
+  const winner = whoWins(userMove, computerMove);
 
-  resultDiv.innerHTML = result;
+  renderResult(winner);
 
+  incrementCounters(winner);
+};
+
+
+
+const renderCounters = () => {
+  counterWinParagraph.innerHTML = `Jugadora: ${playerWinsCounter}`;
+  counterLostParagraph.innerHTML = `Computadora: ${playerLosesCounter}`;
+}
+
+const checkCounters = () => {
   if (playerWinsCounter === 10) {
 
     winnerParagraph.innerHTML = '¡Ha ganado la jugadora!';
     playBtn.classList.add('hidden');
     reloadBtn.classList.remove('hidden');
   }
-
   else if (playerLosesCounter === 10) {
 
     winnerParagraph.innerHTML = '¡Ha ganado la computadora!';
     playBtn.classList.add('hidden');
     reloadBtn.classList.remove('hidden');
   }
-
-
-
-  if (userMove === 'tijera') {
-
-    userSelection(userMove);
-  }
-
-  else if (userMove === 'papel') {
-
-    userSelection(userMove);
-  }
-
-  else if (userMove === 'piedra') {
-
-    userSelection(userMove);
-  }
-
-
-
-  if (computerMove === 'tijera') {
-
-    computerSelection(computerMove);
-  }
-
-  else if (computerMove === 'papel') {
-
-    computerSelection(computerMove);
-  }
-
-  else if (computerMove === 'piedra') {
-
-    computerSelection(computerMove);
-  };
-
-
- /*
-
-  if(userMove === 'tijera') {
-
-    imagePlayerOptionSelect.classList.remove('hidden');
-    imagePlayerOptionSelect.innerHTML = `<img src="./images/imagen_tijera.jpg" alt="La jugadora ha seleccionado tijera">`;
-    paragraphPlayerOptionSelect.classList.remove('hidden');
-    paragraphPlayerOptionSelect.innerHTML = '<p class=h2> La jugadora ha seleccionado tijera</p>';
-
-  }
-  
-  else if(userMove === 'papel') {
-    imagePlayerOptionSelect.classList.remove('hidden');
-    imagePlayerOptionSelect.innerHTML = `<img src="./images/imagen_papel.jpg" alt="La jugadora ha seleccionado papel">`;
-    paragraphPlayerOptionSelect.classList.remove('hidden');
-    paragraphPlayerOptionSelect.innerHTML = '<p class=h2> La jugadora ha seleccionado papel</p>';
-  }
-  
-  else if(userMove === 'piedra') {
-    imagePlayerOptionSelect.classList.remove('hidden');
-    imagePlayerOptionSelect.innerHTML = `<img src="./images/imagen_piedra.jpg" alt="La jugadora ha seleccionado piedra">`;
-    paragraphPlayerOptionSelect.classList.remove('hidden');
-    paragraphPlayerOptionSelect.innerHTML = '<p class=h2> La jugadora ha seleccionado piedra</p>';
-  }
-
-
-
-  if(computerMove === 'tijera') {
-
-    imageComputerOptionSelect.classList.remove('hidden');
-    imageComputerOptionSelect.innerHTML = `<img src="./images/imagen_tijera.jpg" alt="La computadora ha seleccionado tijera">`;
-    paragraphComputerOptionSelect.classList.remove('hidden');
-    paragraphComputerOptionSelect.innerHTML = '<p class=h2> La computadora ha seleccionado tijera</p>';
-
-  }
-  
-  else if(computerMove === 'papel') {
-    imageComputerOptionSelect.classList.remove('hidden');
-    imageComputerOptionSelect.innerHTML = `<img src="./images/imagen_papel.jpg" alt="La computadora ha seleccionado papel">`;
-    paragraphComputerOptionSelect.classList.remove('hidden');
-    paragraphComputerOptionSelect.innerHTML = '<p class=h2> La computadora ha seleccionado papel</p>';
-  }
-  
-  else if(computerMove === 'piedra') {
-    imageComputerOptionSelect.classList.remove('hidden');
-    imageComputerOptionSelect.innerHTML = `<img src="./images/imagen_piedra.jpg" alt="La computadora ha seleccionado piedra">`;
-    paragraphComputerOptionSelect.classList.remove('hidden');
-    paragraphComputerOptionSelect.innerHTML = '<p class=h2> La computadora ha seleccionado piedra</p>';
-
 };
-*/
 
-
+const updateCounters = () => {
+  renderCounters();
+  checkCounters();
 }
 
-const userSelection = (userMove) => {
+const handleClickButton = (ev) => {
+  ev.preventDefault();
 
-  const imageMove = `./images/imagen_${userMove}.jpg`;
-  const altText = `La jugadora ha seleccionado ${userMove}`;
-  const paragraphMove = `<p class="h2">La jugadora ha seleccionado ${userMove}</p>`;
+  playRound();
 
-  imagePlayerOptionSelect.classList.remove('hidden');
-  imagePlayerOptionSelect.innerHTML = `<img src="${imageMove}" alt="${altText}">`;
-
-  paragraphPlayerOptionSelect.classList.remove('hidden');
-  paragraphPlayerOptionSelect.innerHTML = paragraphMove;
-}
-
-const computerSelection = (computerMove) => {
-
-  const imageMove = `./images/imagen_${computerMove}.jpg`;
-  const altText = `La computadora ha seleccionado ${computerMove}`;
-  const paragraphMove = `<p class="h2">La computadora ha seleccionado ${computerMove}</p>`;
-
-  imageComputerOptionSelect.classList.remove('hidden');
-  imageComputerOptionSelect.innerHTML = `<img src="${imageMove}" alt="${altText}">`;
-
-  paragraphComputerOptionSelect.classList.remove('hidden');
-  paragraphComputerOptionSelect.innerHTML = paragraphMove;
+  updateCounters();  
 }
 
 
-
-
-const compareMoves = (userMove, computerMove) => {
-
-
-  // Comparar la jugada de la computadora y la jugada de la usuaria
-
-  // Decir quién ha ganado o si han empatado
-
-  if (userMove === 'select') {
-
-    const result = 'Recuerda seleccionar una jugada';
-
-    return result;
-  }
-
-  if (userMove === computerMove) {
-    // ¡EMPATE!
-
-    const result = 'Has empatado';
-
-    return result;
-  }
-
-  else if ((userMove === 'tijera' && computerMove === 'papel') ||
-    (userMove === 'papel' && computerMove === 'piedra') ||
-    (userMove === 'piedra' && computerMove === 'tijera')) {
-    // Usuaria gana
-
-    const result = 'Has ganado';
-
-    playerWinsCounter += 1;
-
-    counterWinParagraph.innerHTML = 'Jugadora: ' + playerWinsCounter;
-
-    return result;
-  }
-
-  else {
-    // Computadora gana
-
-    const result = 'Has perdido';
-
-    playerLosesCounter += 1;
-
-    counterLostParagraph.innerHTML = 'Computadora: ' + playerLosesCounter;
-
-    return result;
-  }
-}
-
-
-
-
-
-
-// SECCIÓN DE EVENTOS
-
-playBtn.addEventListener('click', handleClickButton);
-
-reloadBtn.addEventListener('click', (ev) => {
+const handleClickResetBtn = (ev) => {
   ev.preventDefault();
 
   playBtn.classList.remove('hidden');
@@ -285,10 +245,18 @@ reloadBtn.addEventListener('click', (ev) => {
   winnerParagraph.innerHTML = '';
   resultDiv.innerHTML = '¡Vamos a jugar!';
 
-}
-)
+  hideMovesElements();
+};
+
+
+
+
+// SECCIÓN DE EVENTOS
+
+playBtn.addEventListener('click', handleClickButton);
+
+reloadBtn.addEventListener('click', handleClickResetBtn);
 
 // CÓDIGO QUE SE EJECUTA AL CARGAR LA PÁGINA
 
-counterWinParagraph.innerHTML = 'Jugadora: 0';
-counterLostParagraph.innerHTML = 'Computadora: 0';
+updateCounters();
